e6a6da2701f35908595201c63e443d98b377365f
AMnatrizio <antonellamanatrizio@hotmail.com>
2024-08-30 00:15:44 -0300

    add new funtions to board and test

--- game/board.py
+++ game/board.py
@@ -4,104 +4,106 @@ from .queen import Queen
 from .bishop import Bishop
 from .rook import Rook
 from .pawn import Pawn
-from .exceptions import InvalidMoveError, PieceNotFoundError, OutOfBoardError
-
+from .piece import Piece
+# from exceptions import InvalidMoveError, PieceNotFoundError, OutOfBoardError
 
 class Board:
 
-    # def __init__(self):
-    #     # Crear un tablero vacío de 8x8
-    #     self.positions = []
-    #     for _ in range(8):
-    #         col = []
-    #         for _ in range(8):
-    #             col.append(None)
-    #         self.positions.append(col)
-
     def __init__(self):
-        # Crear un tablero vacío de 8x8
-        self.positions = [[None for _ in range(8)] for _ in range(8)]
+        # Crear un tablero vacío de 8x8 y agrega las piezas en su posicion.
+        self.matrix = [[None for _ in range(8)] for _ in range(8)]
 
-
-    # Configura las piezas en el tablero
-    def setup_board(self):
-        # Piezas blancas
-        self.place_piece(Rook('white'), 0, 0)  # Torre blanca
-        self.place_piece(Rook('white'), 0, 7)  # Torre blanca
-        self.place_piece(Knight('white'), 0, 1)  # Caballo blanco
-        self.place_piece(Knight('white'), 0, 6)  # Caballo blanco
-        self.place_piece(Bishop('white'), 0, 2)  # Alfil blanco
-        self.place_piece(Bishop('white'), 0, 5)  # Alfil blanco
-        self.place_piece(Queen('white'), 0, 3)  # Reina blanca
-        self.place_piece(King('white'), 0, 4)  # Rey blanco
+        # Blancas
+        self.matrix[0][0] = Rook(color= "WHITE") # Torre
+        self.matrix[0][7] = Rook(color= "WHITE") # Torre
+        self.matrix[0][1] = Knight(color= "WHITE") # Caballo
+        self.matrix[0][6] = Knight(color= "WHITE") # Caballo
+        self.matrix[0][2] = Bishop(color= "WHITE") # Alfil
+        self.matrix[0][5] = Bishop(color= "WHITE") # Alfil
+        self.matrix[0][3] = Queen(color= "WHITE") # Reina
+        self.matrix[0][4] = King(color= "WHITE") # Rey
         for i in range(8):
-            self.place_piece(Pawn('white'), 1, i)  # Peones blancos
+            self.matrix[1][i] = Pawn(color= "WHITE") # Peon
 
-        # Piezas negras
-        self.place_piece(Rook('black'), 7, 0)  # Torre negra
-        self.place_piece(Rook('black'), 7, 7)  # Torre negra
-        self.place_piece(Knight('black'), 7, 1)  # Caballo negro
-        self.place_piece(Knight('black'), 7, 6)  # Caballo negro
-        self.place_piece(Bishop('black'), 7, 2)  # Alfil negro
-        self.place_piece(Bishop('black'), 7, 5)  # Alfil negro
-        self.place_piece(Queen('black'), 7, 3)  # Reina negra
-        self.place_piece(King('black'), 7, 4)  # Rey negro
+        # Negras
+        self.matrix[7][7] = Rook(color= "BLACK") # Torre
+        self.matrix[7][0] = Rook(color= "BLACK") # Torre
+        self.matrix[7][1] = Knight(color= "BLACK") # Caballo
+        self.matrix[7][6] = Knight(color= "BLACK") # Caballo
+        self.matrix[7][2] = Bishop(color= "BLACK") # Alfil
+        self.matrix[7][5] = Bishop(color= "BLACK") # Alfil
+        self.matrix[7][3] = Queen(color= "BLACK") # Reina
+        self.matrix[7][4] = King(color= "BLACK") # Rey
         for i in range(8):
-            self.place_piece(Pawn('black'), 6, i) # Peones negros
-
+            self.matrix[6][i] = Pawn(color= "Black") # Peon
     
-    def place_piece(self, piece, row, col):
-        # Colocar una pieza en la posición indicada
-        self.positions[row][col] = piece
-
-
-    def get_piece(self, position):
-        # Devuelve la pieza en una posición específica
-        row, col = position
-        return self.positions[row][col]
+   # Revisa si la posicion ingresada no sale fuera de los limites del tablero
+    def is_out_of_board(self, row, col):
+        if 0 <= row < 7 and 0 <= col < 7:
+            return True  # Está dentro de los límites del tablero
+        else:
+            return False  # Está fuera de los límites del tablero
     
 
+    def has_piece(self, row, col):
+        if self.matrix[row][col] is not None:
+            return True
 
-    def is_within_bounds(self, position):
-        # Verifica si una posición está dentro del tablero
-        row, col = position
-        return 0 <= row < 8 and 0 <= col < 8
-    
 
-    def move_piece(self, start_position, end_position):
-        # Verificar si las posiciones están dentro de los límites del tablero
-        if not self.is_within_bounds(start_position) or not self.is_within_bounds(end_position):
-            raise OutOfBoardError("El movimiento está fuera de los límites del tablero.")
-
-        # Obtener la pieza en la posición de inicio
-        piece = self.get_piece(start_position)
+    def get_color(self, row, col):
+        piece = self.matrix[row][col]
         if piece is None:
-            raise PieceNotFoundError("No hay ninguna pieza en la posición especificada.")
+            return None
+        else:
+            return piece.color
+        
 
-        # Validar si el movimiento es válido según la lógica de la pieza
-        if not piece.is_valid_move(start_position, end_position, self):
-            raise InvalidMoveError("Movimiento no permitido para esta pieza.")
 
-        # Verificar si hay alguna pieza en el camino, si aplica para la pieza
-        if not self.is_path_clear(start_position, end_position, piece):
-            raise InvalidMoveError("El camino está bloqueado por otra pieza.")
+    # def move_piece(self, start_position, end_position):
+    #     # Verificar si las posiciones están dentro de los límites del tablero
+    #     if not self.is_within_bounds(start_position) or not self.is_within_bounds(end_position):
+    #         raise OutOfBoardError("El movimiento está fuera de los límites del tablero.")
 
-        # Mover la pieza y actualizar las posiciones en el tablero
-        self.place_piece(piece, end_position)
-        self.place_piece(None, start_position)
+    #     # Obtener la pieza en la posición de inicio
+    #     piece = self.get_piece(start_position)
+    #     if piece is None:
+    #         raise PieceNotFoundError("No hay ninguna pieza en la posición especificada.")
+
+    #     # Validar si el movimiento es válido según la lógica de la pieza
+    #     if not piece.is_valid_move(start_position, end_position, self):
+    #         raise InvalidMoveError("Movimiento no permitido para esta pieza.")
+
+    #     # Verificar si hay alguna pieza en el camino, si aplica para la pieza)
+    #     if not self.is_path_clear(start_position, end_position, piece):
+    #         raise InvalidMoveError("El camino está bloqueado por otra pieza.")
+
+    #     # Mover la pieza y actualizar las posiciones en el tablero
+    #     self.place_piece(piece, end_position)
+    #     self.place_piece(None, start_position)
 
 
 
     def display_board(self):
         # Mostrar el tablero en la consola
-        for row in self.positions:
+        print("    0   1   2   3   4   5   6   7")
+        print("  +" + "---+" * 8)
+
+        # Mostrar el tablero con guías de filas (1-8)
+        for i, row in enumerate(self.matrix, start=0):
             row_display = []
             for piece in row:
                 if piece is None:
-                    row_display.append(".")
+                    row_display.append(" ")
                 else:
                     row_display.append(str(piece))
-            print(" ".join(row_display))
+
+            # Imprimir la guía de filas al inicio de cada fila
+            print(f"{i} | " + " | ".join(row_display) + " |")
+            print("  +" + "---+" * 8)
 
 
-        
+
+
+if __name__ == "__main__":
+    board = Board()  # Crear una instancia de la clase Board
+    board.display_board()  # Llamar a la función display_board para imprimir el tablero
--- test/test_board.py
+++ test/test_board.py
@@ -1,11 +1,17 @@
 import unittest
 from game.board import Board
 from game.rook import Rook
+from game.king import King
+from game.knight import Knight
+from game.bishop import Bishop
+from game.queen import Queen
+from game.pawn import Pawn
 
 class TestBoard(unittest.TestCase):
     
     def test_init(self):
-        board = Board() # Creo objeto borad de la Clase Board
+        board = Board() # Creo objeto board de la Clase Board
+        # Testea que se crea el tablero de 8x8
         self.assertEqual(
             len(board.matrix),8)
         for i in range(8):
@@ -16,8 +22,41 @@ class TestBoard(unittest.TestCase):
         # assertIsInstance Verifica si un objeto pertenece a una clase 
         # específica o a una subclase de esa clase
         self.assertIsInstance(board.matrix[0][0],Rook)
+        self.assertIsInstance(board.matrix[0][7],Rook)
+        self.assertIsInstance(board.matrix[0][1],Knight)
+        self.assertIsInstance(board.matrix[0][6],Knight)
+        self.assertIsInstance(board.matrix[0][2],Bishop)
+        self.assertIsInstance(board.matrix[0][5],Bishop)
+        self.assertIsInstance(board.matrix[0][3],Queen)
+        self.assertIsInstance(board.matrix[0][4],King)
+
         # assertEqual Verifica que a es igual a b.
         self.assertEqual(board.matrix[0][0].color,"WHITE")
+        self.assertEqual(board.matrix[7][3].color,"BLACK")
+
+    def test_is_out_of_board(self):
+        board = Board()
+        # Posicion dentro del tablero
+        self.assertTrue(board.is_out_of_board(0,0))
+        # Posicion fuera del tablero
+        self.assertFalse(board.is_out_of_board(9,9))
+
+
+    def test_get_piece_color(self):
+        board = Board()
+        # Una negra
+        color = board.get_color(7, 3)
+        self.assertEqual(color, "BLACK")
+        # Una blanca
+        color = board.get_color(0, 0)
+        self.assertEqual(color, "WHITE")
+        
+
+        
+
+        
+
+
 
 
 
