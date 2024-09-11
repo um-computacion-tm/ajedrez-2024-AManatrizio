d4040f8454ee6cc9d54d26950bc95250d2e1f9b5
AMnatrizio <antonellamanatrizio@hotmail.com>
2024-09-11 01:40:20 -0300

    add is_valid_movement to class king and adapted for the movements of king also add unittest for it

--- game/king.py
+++ game/king.py
@@ -12,7 +12,14 @@ class King(Piece):
     def __str__(self):
         return "♔" if self.color == "WHITE" else "♚"
 
-
-    def move(self):
-        # Lógica para mover el rey
-        return
+    def is_valid_movement(self, initial_row, final_row, initial_col, final_col):
+        # Calcular la diferencia absoluta en filas y columnas
+        row_diff = abs(final_row - initial_row)
+        col_diff = abs(final_col - initial_col)
+        
+        # El rey puede moverse una casilla en cualquier dirección
+        # Por lo tanto, la diferencia máxima en filas y columnas debe ser 1
+        if row_diff <= 1 and col_diff <= 1 and (row_diff != 0 or col_diff != 0):
+            return True
+        
+            
